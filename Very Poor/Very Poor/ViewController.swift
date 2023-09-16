c477be9c77fa15f6c3b8aa3d43a539c2654fac7a
rsmelton <116193054+rsmelton@users.noreply.github.com>
2023-09-15 21:34:06 -0500

    - Added VeryPoor files

---
+++ Very Poor/Very Poor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Very Poor
+//
+//  Created by Scott Melton on 5/5/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
